c42c1370427c3836670f4c092dd4e3888bbaf33e
markgray <mark.t.gray@gmail.com>
2020-06-27 07:00:53 -0400

    Updated gradle dependencies

--- DiceRoller-challenge/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ DiceRoller-challenge/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -16,11 +16,10 @@
 
 package com.example.android.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 /**
--- DiceRollerFinal/app/src/main/java/com/example/android/diceroller/DiceRollerApplication.kt
+++ DiceRollerFinal/app/src/main/java/com/example/android/diceroller/DiceRollerApplication.kt
@@ -3,6 +3,11 @@ package com.example.android.diceroller
 import android.app.Application
 import timber.log.Timber
 
+/**
+ * This override of [Application] allows us to `plant` a [Timber] tree for debug  builds.
+ * Automatically infers the tag from the calling class.
+ */
+@Suppress("unused")
 class DiceRollerApplication : Application() {
 
     override fun onCreate() {
