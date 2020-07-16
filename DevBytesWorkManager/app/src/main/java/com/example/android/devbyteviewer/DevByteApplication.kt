9d460d1207d8af5768bea8266fc3fd8887977f59
markgray <mark.t.gray@gmail.com>
2020-07-16 05:51:52 -0400

    Added comments

--- DevBytesWorkManager/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ DevBytesWorkManager/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -30,21 +30,35 @@ import java.util.concurrent.TimeUnit
 /**
  * Override application to setup background work via WorkManager
  */
+@Suppress("unused") // It is actually used in AndroidManifest.xml
 class DevByteApplication : Application() {
 
+    /**
+     * [CoroutineScope] we use to set up our [WorkManager] periodic refresh of our database using a
+     * background thread from the shared pool of threads of [Dispatchers.Default]
+     */
     private val applicationScope = CoroutineScope(Dispatchers.Default)
 
     /**
-     * onCreate is called before the first screen is shown to the user.
+     * [onCreate] is called before the first screen is shown to the user.
      *
      * Use it to setup any background tasks, running expensive setup operations in a background
      * thread to avoid delaying app start.
+     *
+     * First we call our super's implementation of `onCreate`, then we call our [delayedInit] method
+     * to add a new logging tree for debug builds to [Timber], and call our [setupRecurringWork]
+     * method to set up our [WorkManager] periodic refresh of our database from the network.
      */
     override fun onCreate() {
         super.onCreate()
         delayedInit()
     }
 
+    /**
+     * Launches a lambda on our [CoroutineScope] field [applicationScope] which calls the [Timber.plant]
+     * method to add a new logging tree for debug builds to [Timber], and calls our [setupRecurringWork]
+     * to set up our [WorkManager] periodic refresh of our database from the network.
+     */
     private fun delayedInit() {
         applicationScope.launch {
             Timber.plant(Timber.DebugTree())
@@ -53,7 +67,24 @@ class DevByteApplication : Application() {
     }
 
     /**
-     * Setup WorkManager background job to 'fetch' new network data daily.
+     * Setup WorkManager background job to 'fetch' new network data daily. We initialize our
+     * [Constraints] variable `val constraints` to the results of using a [Constraints.Builder]
+     * instance to set the required network type to [NetworkType.UNMETERED], require the battery
+     * to be charging, require that the battery not be low, if the build version is greater than
+     * "M" require that the device is idle, and then building that [Constraints.Builder].
+     *
+     * We initialize our [PeriodicWorkRequest] variable `val repeatingRequest` by using a
+     * [PeriodicWorkRequestBuilder] for our [CoroutineWorker] class [RefreshDataWorker] with a
+     * repeat interval of 1 days, adding `constraints` as the constraints for the work then
+     * building that [PeriodicWorkRequest.Builder].
+     *
+     * We then log that "WorkManager: Periodic Work request for sync is scheduled". We then retrieve
+     * the default singleton instance of [WorkManager], and use its `enqueueUniquePeriodicWork`
+     * method to enqueue a [PeriodicWorkRequest] with the unique name [RefreshDataWorker.WORK_NAME],
+     * an existing periodic work policy of [ExistingPeriodicWorkPolicy.KEEP] (if there is existing
+     * pending (uncompleted) work with the same unique name, do nothing. Otherwise, insert the new
+     * work), and `repeatingRequest` as the [PeriodicWorkRequest] to enqueue.
+     *
      */
     private fun setupRecurringWork() {
 
@@ -68,9 +99,10 @@ class DevByteApplication : Application() {
                 }
                 .build()
 
-        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
-                .setConstraints(constraints)
-                .build()
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(
+                1,
+                TimeUnit.DAYS
+        ).setConstraints(constraints).build()
 
         Timber.d("WorkManager: Periodic Work request for sync is scheduled")
         WorkManager.getInstance().enqueueUniquePeriodicWork(
