3bc7912cb98d06d6a4aeeef6647406ccc3ee67f3
markgray <mark.t.gray@gmail.com>
2020-07-22 05:27:32 -0400

    Added comments

--- GDGFinderFinal/app/src/main/java/com/example/android/gdgfinder/add/AddGdgViewModel.kt
+++ GDGFinderFinal/app/src/main/java/com/example/android/gdgfinder/add/AddGdgViewModel.kt
@@ -18,28 +18,31 @@ package com.example.android.gdgfinder.add
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModel
 
+/**
+ * [ViewModel] "controlling" our `AddGdgFragment` fragment.
+ */
 class AddGdgViewModel : ViewModel() {
 
     /**
-     * Request a toast by setting this value to true.
-     *
-     * This is private because we don't want to expose setting this value to the Fragment.
+     * Request a `Snackbar` by setting this value to true. This is private because we don't want to
+     * expose setting this value to the Fragment, the public read only version is our property
+     * [showSnackBarEvent].
      */
     private var _showSnackbarEvent = MutableLiveData<Boolean?>()
 
     /**
-     * If this is true, immediately `show()` a toast and call `doneShowingSnackbar()`.
+     * If this is true, immediately `show()` a `Snackbar` and call `doneShowingSnackbar()`.
      */
     val showSnackBarEvent: LiveData<Boolean?>
         get() = _showSnackbarEvent
 
     /**
-     * Call this immediately after calling `show()` on a toast.
+     * Call this immediately after calling `show()` on a `Snackbar`.
      *
-     * It will clear the toast request, so if the user rotates their phone it won't show a duplicate
-     * toast.
+     * It will clear the `Snackbar` request, so if the user rotates their phone it won't show a
+     * duplicate toast.
      */
     fun doneShowingSnackbar() {
         _showSnackbarEvent.value = null
