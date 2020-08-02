75c23a9be24dbf95ebc9b7c875c91850f584e525
markgray <mark.t.gray@gmail.com>
2020-08-02 05:58:54 -0400

    Added comments

--- GuessTheWordViewModel/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ GuessTheWordViewModel/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -24,7 +24,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
@@ -34,12 +34,57 @@ import com.example.android.guesstheword.databinding.GameFragmentBinding
  */
 class GameFragment : Fragment() {
 
+    /**
+     * [GameFragmentBinding] inflated from our layout file [R.layout.game_fragment]/
+     */
     private lateinit var binding: GameFragmentBinding
 
+    /**
+     * Handle to our singleton [GameViewModel] view model
+     */
     private lateinit var viewModel: GameViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    /**
+     * Called to have the fragment instantiate its user interface view. This will be called between
+     * [onCreate] and [onActivityCreated]. It is recommended to only inflate the layout in this
+     * method and move logic that operates on the returned View to [onViewCreated]. We have the
+     * [DataBindingUtil.inflate] method use our [LayoutInflater] parameter [inflater] to inflate
+     * our layout file [R.layout.game_fragment] using our [ViewGroup] parameter [container] for the
+     * LayoutParams without attaching to it and use the [GameFragmentBinding] binding to the result
+     * that it returns to initialize our field [binding]. We create `ViewModelProvider` with `this`
+     * as the `ViewModelStoreOwner` and call its `get` method to retrieve an existing ViewModel or
+     * create a new one and initialize our [GameViewModel] field [viewModel] to the view model it
+     * returns.
+     *
+     * We set the `OnClickListener` of the `correctButton` property of [binding] (the button with
+     * ID [R.id.correct_button] in our layout file labeled "Got it") to a lambda that calls our
+     * method [onCorrect], we set the `OnClickListener` of the `skipButton` property of [binding]
+     * (the button with ID [R.id.skip_button] in our layout file labeled "Skip") to a lambda that
+     * calls our method [onSkip], and we set the `OnClickListener` of the `endGameButton` property
+     * of [binding] (the button with ID [R.id.end_game_button] in our layout file labeled "End game")
+     * to a lambda that calls our method [onEndGame].
+     *
+     * We call our method [updateScoreText] to set the text of `wordText` property of [binding] to
+     * the `word` property of [viewModel] (the current word to be quessed), and we call our method
+     * [updateWordText] to set the text of `scoreText` property of [binding] to the string value of
+     * the `score` property of [viewModel] (the current score).
+     *
+     * Finally we return the outermost View in the layout file associated with [binding] to our
+     * caller.
+     *
+     * @param inflater The [LayoutInflater] object that can be used to inflate XML layout files.
+     * @param container If non-null, this is the parent view that the fragment's UI will be
+     * attached to.  The fragment should not add the view itself, but this can be used to
+     * generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous
+     * saved state as given here.
+     * @return Return the [View] for the fragment's UI, or `null`.
+     */
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
 
         // Inflate view and obtain an instance of the binding class
         binding = DataBindingUtil.inflate(
@@ -48,10 +93,10 @@ class GameFragment : Fragment() {
                 container,
                 false
         )
-        Log.i("GameFragment", "Called ViewModelProviders.of")
+        Log.i("GameFragment", "Called ViewModelProvider")
 
         // Get the viewModel
-        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
         binding.correctButton.setOnClickListener { onCorrect() }
         binding.skipButton.setOnClickListener { onSkip() }
@@ -63,11 +108,30 @@ class GameFragment : Fragment() {
 
     /** Methods for buttons presses **/
 
+    /**
+     * This is called when the "Skip" button is clicked (`skipButton` property of [binding], aka
+     * resource ID [R.id.skip_button] in our layout file). First we call the `onSkip` method of
+     * [GameViewModel] field [viewModel] which decrements our score by 1 and advances to the next
+     * word. Then we call our method [updateWordText] to update the text of the `wordText`
+     * `TextView` property of [binding] to the new `word` of our [viewModel], and we call our
+     * method [updateScoreText] to update the text of the `scoreText` `TextView` property of
+     * [binding] to the new `score` of [viewModel].
+     */
     private fun onSkip() {
         viewModel.onSkip()
         updateWordText()
         updateScoreText()
     }
+
+    /**
+     * This is called when the "Got it" button is clicked (`correctButton` property of [binding],
+     * aka resource ID [R.id.correct_button] in our layout file). First we call the `onCorrect`
+     * method of [GameViewModel] field [viewModel] which increments our score by 1 and advances
+     * to the next word. Then we call our method [updateWordText] to update the text of the
+     * `wordText` `TextView` property of [binding] to the new `word` of our [viewModel], and we
+     * call our method [updateScoreText] to update the text of the `scoreText` `TextView` property
+     * of [binding] to the new `score` of [viewModel].
+     */
     private fun onCorrect() {
         viewModel.onCorrect()
         updateScoreText()
