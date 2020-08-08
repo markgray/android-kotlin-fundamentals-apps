3d0aaf66beb4150679f521927c4beb01ec427506
markgray <mark.t.gray@gmail.com>
2020-08-08 05:34:37 -0400

    Added comments

--- MarsRealEstateGrid/app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
+++ MarsRealEstateGrid/app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
@@ -18,9 +18,13 @@
 package com.example.android.marsrealestate.overview
 
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.example.android.marsrealestate.R
 import com.example.android.marsrealestate.databinding.FragmentOverviewBinding
 
@@ -33,20 +37,44 @@ class OverviewFragment : Fragment() {
      * Lazily initialize our [OverviewViewModel].
      */
     private val viewModel: OverviewViewModel by lazy {
-        ViewModelProviders.of(this).get(OverviewViewModel::class.java)
+        ViewModelProvider(this).get(OverviewViewModel::class.java)
     }
 
     /**
-     * Inflates the layout with Data Binding, sets its lifecycle owner to the OverviewFragment
-     * to enable Data Binding to observe LiveData, and sets up the RecyclerView with an adapter.
+     * Called to have the fragment instantiate its user interface view. This will be called between
+     * [onCreate] and [onActivityCreated]. We initialize our [FragmentOverviewBinding] variable
+     * `val binding` by having the [FragmentOverviewBinding.inflate] method use our [LayoutInflater]
+     * parameter [inflater] to inflate the layout file it is associated with (the layout file
+     * layout/fragment_overview.xml) into a [FragmentOverviewBinding] instance. We set the
+     * LifecycleOwner that should be used for observing changes of LiveData in `binding` to `this`
+     * [OverviewFragment], and set the `viewModel` variable property of `binding` to our
+     * [OverviewViewModel] field [viewModel] (gives the binding expressions in `binding` access to
+     * our [OverviewViewModel]). We then set the adapter of the `photosGrid` RecyclerView in
+     * `binding` to a new instance of [PhotoGridAdapter], and we call [setHasOptionsMenu] with
+     * `true` to report that this fragment would like to participate in populating the options menu.
+     * Finally we return the outermost View in the layout file associated with `binding` to the
+     * caller.
+     *
+     * @param inflater The [LayoutInflater] object that can be used to inflate
+     * any views in the fragment.
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI will be attached to. The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return Return the [View] for the fragment's UI, or null.
      */
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
         val binding = FragmentOverviewBinding.inflate(inflater)
         //val binding = GridViewItemBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
-        binding.setLifecycleOwner(this)
+        binding.lifecycleOwner = this
 
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
@@ -59,7 +87,12 @@ class OverviewFragment : Fragment() {
     }
 
     /**
-     * Inflates the overflow menu that contains filtering options.
+     * Inflates the overflow menu that contains filtering options. We just use our [MenuInflater]
+     * parameter [inflater] to inflate our layout file menu/overflow_menu.xml into our [Menu]
+     * parameter [menu] and then call our super's implementation of `onCreateOptionsMenu`.
+     *
+     * @param menu The options menu in which you place your items.
+     * @param inflater a [MenuInflater] you can use to inflate an XML file into a menu.
      */
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.overflow_menu, menu)
