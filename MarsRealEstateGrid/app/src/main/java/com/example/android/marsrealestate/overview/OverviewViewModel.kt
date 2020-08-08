9c46fcaef60c9d262a0f9c5fe285d19e0049b990
markgray <mark.t.gray@gmail.com>
2020-08-08 06:31:54 -0400

    Added comments

--- MarsRealEstateGrid/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ MarsRealEstateGrid/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -27,6 +27,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
+/**
+ * The possible status states of the most recent request. [LOADING] is used after our method
+ * `getMarsRealEstateProperties` calls `Retrofit` method `getProperties`, [ERROR] is used if
+ * an error occurs while waiting for the result, and [DONE] is used if the download completes
+ * successfully.
+ */
 enum class MarsApiStatus { LOADING, ERROR, DONE }
 
 /**
@@ -34,18 +40,39 @@ enum class MarsApiStatus { LOADING, ERROR, DONE }
  */
 class OverviewViewModel : ViewModel() {
 
-    // The internal MutableLiveData that stores the status of the most recent request
+    /**
+     * The internal MutableLiveData that stores the status of the most recent request, [status] is
+     * the public read-only access to this value. Set to [MarsApiStatus.LOADING] when our method
+     * [getMarsRealEstateProperties] calls `Retrofit` method `getProperties` to begin the download
+     * of the list of [MarsProperty], set to [MarsApiStatus.ERROR] is an [Exception] is thrown, and
+     * to [MarsApiStatus.DONE] if the download completes successfully.
+     */
     private val _status = MutableLiveData<MarsApiStatus>()
 
-    // The external immutable LiveData for the request status
+    /**
+     * The external immutable LiveData for the request status. The `bindStatus` binding adapter for
+     * the "app:marsApiStatus" attribute accesses this property and uses the value to control the
+     * visibility and image displayed by any `ImageView` which uses the attribute "app:marsApiStatus"
+     * with the [status] property of this [OverviewViewModel] in a binding expression. The `ImageView`
+     * with resource ID `R.id.status_image` in the layout file layout/fragment_overview.xml does so.
+     */
     val status: LiveData<MarsApiStatus>
         get() = _status
 
-    // Internally, we use a MutableLiveData, because we will be updating the List of MarsProperty
-    // with new values
+    /**
+     * The list of [MarsProperty] objects downloaded by `Retrofit` from the internet. Internally, we
+     * use a MutableLiveData, because we will be updating the List of [MarsProperty] with new values.
+     */
     private val _properties = MutableLiveData<List<MarsProperty>>()
 
-    // The external LiveData interface to the property is immutable, so only this class can modify
+    /**
+     * The external LiveData interface to the [_properties] property. Immutable, so only this class
+     * can modify. A binding expression for the "app:listData" attribute accesses this property when
+     * the attribute is used for a `RecyclerView` as the `RecyclerView` with ID `R.id.photos_grid`
+     * in the layout file layout/fragment_overview.xml does. "app:listData" is handled by the
+     * binding adapter `bindRecyclerView` which retrieves the adapter of the `RecyclerView` and
+     * calls its `submitList` method to submit the [properties] list to be diffed, and displayed.
+     */
     val properties: LiveData<List<MarsProperty>>
         get() = _properties
 
@@ -71,6 +98,7 @@ class OverviewViewModel : ViewModel() {
         coroutineScope.launch {
             _status.value = MarsApiStatus.LOADING
             // this will run on a thread managed by Retrofit
+            @Suppress("CanBeVal")
             var getPropertiesDeferred = MarsApi.retrofitService.getProperties()
             try {
                 _status.value = MarsApiStatus.LOADING
