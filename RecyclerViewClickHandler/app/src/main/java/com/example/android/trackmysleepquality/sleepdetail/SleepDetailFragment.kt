4aa92d47bb24a2dca8beb66423c4470b1aa5d2e4
markgray <mark.t.gray@gmail.com>
2020-08-30 06:16:30 -0400

    Added comments

--- RecyclerViewClickHandler/app/src/main/java/com/example/android/trackmysleepquality/sleepdetail/SleepDetailFragment.kt
+++ RecyclerViewClickHandler/app/src/main/java/com/example/android/trackmysleepquality/sleepdetail/SleepDetailFragment.kt
@@ -16,17 +16,20 @@
 
 package com.example.android.trackmysleepquality.sleepdetail
 
+import android.app.Application
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 import com.example.android.trackmysleepquality.database.SleepNight
 import com.example.android.trackmysleepquality.databinding.FragmentSleepDetailBinding
 
@@ -38,6 +41,46 @@ import com.example.android.trackmysleepquality.databinding.FragmentSleepDetailBi
  */
 class SleepDetailFragment : Fragment() {
 
+    /**
+     * Called to have the fragment instantiate its user interface view. First we use the
+     * [DataBindingUtil.inflate] method to inflate our layout file R.layout.fragment_sleep_detail.
+     * Then we initialize our [Application] variable `val application` to the application that owns
+     * this activity, and our [SleepDetailFragmentArgs] variable `val arguments` to the instance
+     * that the [SleepDetailFragmentArgs.fromBundle] method creates from the arguments supplied
+     * when the fragment was instantiated (the safe args [Long] for the `sleepNightKey` argument
+     * will be stored in the `sleepNightKey` property of `arguments`). We initialize our
+     * [SleepDatabaseDao] variable `val dataSource` to the `sleepDatabaseDao` property of our
+     * singleton [SleepDatabase] instance (creating the database if need be) (`dataSource` will
+     * allow us to access the Room SQLite methods defined in the [SleepDatabaseDao]). We initialize
+     * our [SleepDetailViewModelFactory] with an instance constructed to use the `sleepNightKey`
+     * entry of `arguments` as the `nightId` of the `SleepNight` row, and `dataSource` as the
+     * [SleepDatabaseDao]. We then use `viewModelFactory` as the Factory which will be used to
+     * instantiate new ViewModels in a call to the [ViewModelProvider.get] method in order to
+     * initialize our [SleepDetailViewModel] variable `val sleepDetailViewModel` to our
+     * singleton [SleepDetailViewModel]. We set the `sleepDetailViewModel` variable of `binding`
+     * to this `sleepDetailViewModel` (this will allow DataBinding binding expressions in the
+     * associated layout file to use the [LiveData] of our ViewModel). We set the `lifecycleOwner`
+     * of `binding` to `this` (Sets the LifecycleOwner that should be used for observing changes
+     * of LiveData in the binding). We add an [Observer] to the `navigateToSleepTracker` [LiveData]
+     * wrapped [Boolean] property of `sleepDetailViewModel` whose lambda will, if the property
+     * transitions to `true`, find our `NavController` and use it to navigate to `SleepTrackerFragment`,
+     * and then call the `doneNavigating` method of `sleepDetailViewModel` to reset
+     * `navigateToSleepTracker` to `false` to make sure we only navigate once, even if the device
+     * has a configuration change.
+     *
+     * Finally we return the outermost [View] in the layout file associated with `binding` to the
+     * caller.
+     *
+     * @param inflater The [LayoutInflater] object that can be used to inflate
+     * any views in the fragment.
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI will be attached to. The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return Return the [View] for the fragment's UI, or null.
+     */
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -52,11 +95,11 @@ class SleepDetailFragment : Fragment() {
                 false
         )
 
-        val application = requireNotNull(this.activity).application
+        val application: Application = requireNotNull(this.activity).application
         val arguments = SleepDetailFragmentArgs.fromBundle(requireArguments())
 
         // Create an instance of the ViewModel Factory.
-        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+        val dataSource: SleepDatabaseDao = SleepDatabase.getInstance(application).sleepDatabaseDao
         val viewModelFactory = SleepDetailViewModelFactory(arguments.sleepNightKey, dataSource)
 
         // Get a reference to the ViewModel associated with this fragment.
--- RecyclerViewClickHandler/app/src/main/java/com/example/android/trackmysleepquality/sleepdetail/SleepDetailViewModel.kt
+++ RecyclerViewClickHandler/app/src/main/java/com/example/android/trackmysleepquality/sleepdetail/SleepDetailViewModel.kt
@@ -17,57 +17,86 @@
 package com.example.android.trackmysleepquality.sleepdetail
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 import com.example.android.trackmysleepquality.database.SleepNight
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 /**
- * ViewModel for SleepQualityFragment.
+ * ViewModel for SleepDetailFragment.
  *
  * @param sleepNightKey The key of the current night we are working on.
+ * @param dataSource Handle to the [SleepDatabaseDao] to use to call its Room SQLite methods.
  */
 class SleepDetailViewModel(
         private val sleepNightKey: Long = 0L,
-        dataSource: SleepDatabaseDao) : ViewModel() {
+        dataSource: SleepDatabaseDao
+) : ViewModel() {
 
     /**
-     * Hold a reference to SleepDatabase via its SleepDatabaseDao.
+     * Hold a reference to `SleepDatabase` via its [SleepDatabaseDao].
      */
     val database = dataSource
 
     /** Coroutine setup variables */
 
     /**
-     * viewModelJob allows us to cancel all coroutines started by this ViewModel.
+     * viewModelJob allows us to cancel all coroutines started by this ViewModel ... BUT we don't
+     * start any coroutines so it is not actually of any use.
      */
     private val viewModelJob = Job()
 
+    /**
+     * The [SleepNight] whose primary key is [sleepNightKey] whose detail the `SleepDetailFragment`
+     * is displaying. Set by a call to the `getNightWithId` method of our [SleepDatabaseDao] field
+     * [database] in our `init` block. It is used in the layout/fragment_sleep_detail.xml layout
+     * file in a binding expression for the "app:sleepImage" attribute of the `ImageView` with ID
+     * R.id.quality_image (the BindingAdapter for this attribute is the extension function for
+     * `ImageView` in BindingUtils.kt `setSleepImage` which sets the drawable of the `ImageView` to
+     * one chosen based on the `sleepQuality` property of [night]), in a binding expression for the
+     * "app:sleepQualityString" attribute of the `TextView` with ID R.id.quality_string (the
+     * BindingAdapter for this attribute is the extension function for `TextView` in BindingUtils.kt
+     * `setSleepQualityString` which sets the text of the `TextView` to one formatted based on the
+     * `sleepQuality` property of [night]), and in a binding expression for the "app:sleepDurationFormatted"
+     * attribute of the `TextView` with ID R.id.sleep_length (the BindingAdapter for this attribute
+     * is the extension function for `TextView` in BindingUtils.kt `setSleepDurationFormatted` which
+     * sets the text of the `TextView` to one formatted based on the `startTimeMilli` and `endTimeMilli`
+     * properties of [night]).
+     */
     private val night: LiveData<SleepNight>
 
+    /**
+     * Getter for our [night] field, it is used by binding expressions in the layout file
+     * layout/fragment_sleep_detail.xml
+     */
     fun getNight() = night
 
 
     init {
+        /**
+         * Initialize our `SleepNight` field `night` from the Room database.
+         */
         night=database.getNightWithId(sleepNightKey)
     }
 
     /**
-     * Variable that tells the fragment whether it should navigate to [SleepTrackerFragment].
-     *
-     * This is `private` because we don't want to expose the ability to set [MutableLiveData] to
-     * the [Fragment]
+     * Variable that tells the fragment whether it should navigate to `SleepTrackerFragment`. This
+     * is `private` because we don't want to expose the ability to set [MutableLiveData] to the
+     * `Fragment`. Set to `true` by our [onClose] method which is called by a binding expression for
+     * the "android:onClick" attribute of the "CLOSE" `Button` in the layout/fragment_sleep_detail.xml
+     * layout file. Set to `null` by our [doneNavigating] method which is called by an Observer of
+     * [navigateToSleepTracker] added to it in the `onCreateView` override of `SleepDetailFragment`
+     * after it navigates to `SleepTrackerFragment`. Public read-only access is provided by our
+     * [navigateToSleepTracker] property.
      */
     private val _navigateToSleepTracker = MutableLiveData<Boolean?>()
 
     /**
-     * When true immediately navigate back to the [SleepTrackerFragment]
+     * When true immediately navigate back to the `SleepTrackerFragment`. Public read-only access to
+     * our [_navigateToSleepTracker] property. An Observer is added to it in the `onCreateView`
+     * override of `SleepDetailFragment` which navigates to `SleepTrackerFragment` when it transitions
+     * to `true`.
      */
     val navigateToSleepTracker: LiveData<Boolean?>
         get() = _navigateToSleepTracker
@@ -84,7 +113,7 @@ class SleepDetailViewModel(
 
 
     /**
-     * Call this immediately after navigating to [SleepTrackerFragment]
+     * Call this immediately after navigating to `SleepTrackerFragment`
      */
     fun doneNavigating() {
         _navigateToSleepTracker.value = null
