2d737d44956dc57ca5b87820dc52402da0d2e34f
markgray <mark.t.gray@gmail.com>
2020-09-17 06:32:04 -0400

    Added comments

--- TrackMySleepQualityFinal/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ TrackMySleepQualityFinal/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -17,11 +17,11 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
 import android.app.Application
+import android.text.Spanned
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 import com.example.android.trackmysleepquality.database.SleepNight
 import com.example.android.trackmysleepquality.formatNights
@@ -32,11 +32,15 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 /**
- * ViewModel for SleepTrackerFragment.
+ * ViewModel for `SleepTrackerFragment`.
+ *
+ * @param database the [SleepDatabaseDao] to use to access the database
+ * @param application the [Application] to use to access resources
  */
 class SleepTrackerViewModel(
         val database: SleepDatabaseDao,
-        application: Application) : AndroidViewModel(application) {
+        application: Application
+) : AndroidViewModel(application) {
 
     /** Coroutine variables */
 
@@ -46,44 +50,68 @@ class SleepTrackerViewModel(
     private var viewModelJob = Job()
 
     /**
-     * A [CoroutineScope] keeps track of all coroutines started by this ViewModel.
-     *
-     * Because we pass it [viewModelJob], any coroutine started in this uiScope can be cancelled
-     * by calling `viewModelJob.cancel()`
-     *
-     * By default, all coroutines started in uiScope will launch in [Dispatchers.Main] which is
-     * the main thread on Android. This is a sensible default because most coroutines started by
-     * a [ViewModel] update the UI after performing some processing.
+     * A [CoroutineScope] that keeps track of all coroutines started by this ViewModel. Because we
+     * pass it [viewModelJob], any coroutine started in this scope can be cancelled by calling
+     * `viewModelJob.cancel()`. By default, all coroutines started in [uiScope] will launch in
+     * [Dispatchers.Main] which is the main thread on Android. This is a sensible default because
+     * most coroutines started by a `ViewModel` update the UI after performing some processing.
      */
     private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
+    /**
+     * The latest [SleepNight] read back from the database if a sleep recording is in progress (the
+     * `endTimeMilli` and `startTimeMilli` fields are equal), or `null` if we are not recording a
+     * sleep quality (the last [SleepNight] entered that we read by calling the `getTonight` method
+     * of [database] had different `endTimeMilli` and `startTimeMilli` fields). When non-null we
+     * update it in our [onStopTracking] method when the STOP button is clicked.
+     */
     private var tonight = MutableLiveData<SleepNight?>()
 
+    /**
+     * The [LiveData] wrapped list of all of the [SleepNight] entries read from the database.
+     */
     private val nights = database.getAllNights()
 
     /**
-     * Converted nights to Spanned for displaying.
+     * Converted [nights] to [Spanned] for displaying. We use the [Transformations.map] method to
+     * convert our [LiveData] wrapped list of [SleepNight]'s field [nights] into a [LiveData]
+     * wrapped [Spanned] by applying our [formatNights] method to the value in [nights].
      */
     val nightsString = Transformations.map(nights) { nights ->
         formatNights(nights, application.resources)
     }
 
     /**
-     * If tonight has not been set, then the START button should be visible.
+     * If tonight has not been set, then the START button should be visible. We use the
+     * [Transformations.map] method to convert our [LiveData] wrapped [SleepNight] field
+     * [tonight] to a [LiveData] wrapped [Boolean] which is `true` if [tonight] is `null`.
+     * Used in a binding expression for the "android:enabled" attribute of the `Button` with
+     * ID R.id.start_button in the file layout/fragment_sleep_tracker.xml (the layout file for
+     * `SleepTrackerFragment`).
      */
     val startButtonVisible = Transformations.map(tonight) {
         null == it
     }
 
     /**
-     * If tonight has been set, then the STOP button should be visible.
+     * If tonight has been set, then the STOP button should be visible. We use the
+     * [Transformations.map] method to convert our [LiveData] wrapped [SleepNight] field
+     * [tonight] to a [LiveData] wrapped [Boolean] which is `true` if [tonight] is not `null`.
+     * Used in a binding expression for the "android:enabled" attribute of the `Button` with
+     * ID R.id.stop_button in the file layout/fragment_sleep_tracker.xml (the layout file for
+     * `SleepTrackerFragment`).
      */
     val stopButtonVisible = Transformations.map(tonight) {
         null != it
     }
 
     /**
-     * If there are any nights in the database, show the CLEAR button.
+     * If there are any nights in the database, show the CLEAR button. We use the
+     * [Transformations.map] method to convert our [LiveData] wrapped list of [SleepNight] field
+     * [nights] to a [LiveData] wrapped [Boolean] which is `true` if [tonight] is not empty.
+     * Used in a binding expression for the "android:enabled" attribute of the `Button` with
+     * ID R.id.clear_button in the file layout/fragment_sleep_tracker.xml (the layout file for
+     * `SleepTrackerFragment`).
      */
     val clearButtonVisible = Transformations.map(nights) {
         it?.isNotEmpty()
@@ -91,43 +119,55 @@ class SleepTrackerViewModel(
 
 
     /**
-     * Request a toast by setting this value to true.
-     *
-     * This is private because we don't want to expose setting this value to the Fragment.
+     * Request a `Snackbar` by setting this value to true. This is private because we don't want to
+     * expose setting this value to the Fragment, publicly available read-only access is provided
+     * by [showSnackBarEvent]. This is set to `true` by our [onClear] method and set to `null` by
+     * our [doneShowingSnackbar] method. [onClear] is called by a binding expression for the
+     * "android:onClick" attribute of the "Clear" button (the `Button` with ID R.id.clear_button in
+     * the file layout/fragment_sleep_tracker.xml), and [doneShowingSnackbar] is called by
+     * the `Observer` added to [showSnackBarEvent] after it shows the Snackbar.
      */
     private var _showSnackbarEvent = MutableLiveData<Boolean>()
 
     /**
-     * If this is true, immediately `show()` a toast and call `doneShowingSnackbar()`.
+     * If this is true, immediately `show()` a Snackbar and call [doneShowingSnackbar] to reset to
+     * `null`. An `Observer` is added to it in the `onCreateView` override of `SleepTrackerFragment`
+     * which shows a Snackbar informing the user "All your data is gone forever"
      */
     val showSnackBarEvent: LiveData<Boolean>
         get() = _showSnackbarEvent
 
     /**
-     * Variable that tells the Fragment to navigate to a specific [SleepQualityFragment]
-     *
-     * This is private because we don't want to expose setting this value to the Fragment.
+     * Variable that tells the Fragment to navigate to `SleepQualityFragment` using the specified
+     * [SleepNight] as the safe args for the fragment. This is private because we don't want to
+     * expose setting this value to the Fragment. Set to the value of our [tonight] if it is not
+     * `null` by our [onStopTracking] method which is called by a binding expression for the
+     * "android:onClick" attribute of the "Stop" button (the `Button` with ID R.id.stop_button in
+     * the file layout/fragment_sleep_tracker.xml). Set to `null` by our [doneNavigating] method
+     * which is called after navigating to the `SleepQualityFragment` to prevent repeated navigating.
      */
     private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
 
     /**
-     * If this is non-null, immediately navigate to [SleepQualityFragment] and call [doneNavigating]
+     * If this is non-null, immediately navigate to `SleepQualityFragment` and call [doneNavigating].
+     * An `Observer` is added to it in the `onCreateView` override of `SleepTrackerFragment` which
+     * navigates to `SleepQualityFragment` using the `nightId` primary key of the [SleepNight] we
+     * hold as the safe args to pass.
      */
     val navigateToSleepQuality: LiveData<SleepNight>
         get() = _navigateToSleepQuality
 
     /**
-     * Call this immediately after calling `show()` on a toast.
-     *
-     * It will clear the toast request, so if the user rotates their phone it won't show a duplicate
-     * toast.
+     * Call this immediately after calling `show()` on a Snackbar. It will clear the Snackbar request,
+     * so if the user rotates their phone it won't show a duplicate Snackbar. Called by the `Observer`
+     * added to our [showSnackBarEvent] field after it shows its Snackbar.
      */
     fun doneShowingSnackbar() {
         _showSnackbarEvent.value = false
     }
 
     /**
-     * Call this immediately after navigating to [SleepQualityFragment]
+     * Call this immediately after navigating to `SleepQualityFragment`
      *
      * It will clear the navigation request, so if the user rotates their phone it won't navigate
      * twice.
