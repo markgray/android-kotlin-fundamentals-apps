8c9479ab881d6152fc584d3c3c93d95f0a9e0944
markgray <mark.t.gray@gmail.com>
2020-12-10 08:24:19 -0500

    Updated gradle dependencies - need to switch to kotlin-parcelize next

--- GDGFinderFinal/app/src/main/java/com/example/android/gdgfinder/add/AddGdgFragment.kt
+++ GDGFinderFinal/app/src/main/java/com/example/android/gdgfinder/add/AddGdgFragment.kt
@@ -73,6 +73,7 @@ class AddGdgFragment : Fragment() {
      *
      * @return Return the [View] for the fragment's UI, or null.
      */
+    @Suppress("RedundantNullableReturnType")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
--- GDGFinderFinal/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ GDGFinderFinal/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -76,6 +76,7 @@ class HomeFragment : Fragment() {
      *
      * @return Return the [View] for the fragment's UI.
      */
+    @Suppress("RedundantNullableReturnType")
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
