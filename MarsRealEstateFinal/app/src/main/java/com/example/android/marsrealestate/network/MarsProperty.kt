0cf1be7deb543c816df70cbfafc82b0232d23c0b
markgray <mark.t.gray@gmail.com>
2020-12-12 07:48:29 -0500

    Updated gradle dependencies MarsRealEstateFinal replaced plugin 'kotlin-android-extensions' with 'kotlin-parcelize' and import kotlinx.android.parcel.Parcelize with import kotlinx.parcelize.Parcelize

--- MarsRealEstateFinal/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ MarsRealEstateFinal/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -19,7 +19,7 @@ package com.example.android.marsrealestate.network
 
 import android.os.Parcelable
 import com.squareup.moshi.Json
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 /**
  * This data class defines a Mars property which includes an ID, the image URL, the type (sale
