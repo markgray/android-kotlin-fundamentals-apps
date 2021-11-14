25c74426e1eefbc091106f432de6625b304c146f
markgray <mark.t.gray@gmail.com>
2021-11-14 06:17:22 -0500

    Updated to SDK 31 in DevBytesWorkManager

--- DevBytesWorkManager/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ DevBytesWorkManager/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -89,25 +89,25 @@ class DevByteApplication : Application() {
     private fun setupRecurringWork() {
 
         val constraints = Constraints.Builder()
-                .setRequiredNetworkType(NetworkType.UNMETERED)
-                .setRequiresCharging(true)
-                .setRequiresBatteryNotLow(true)
-                .apply {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        setRequiresDeviceIdle(true)
-                    }
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresCharging(true)
+            .setRequiresBatteryNotLow(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
                 }
-                .build()
+            }
+            .build()
 
         val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(
-                1,
-                TimeUnit.DAYS
+            1,
+            TimeUnit.DAYS
         ).setConstraints(constraints).build()
 
         Timber.d("WorkManager: Periodic Work request for sync is scheduled")
-        WorkManager.getInstance().enqueueUniquePeriodicWork(
-                RefreshDataWorker.WORK_NAME,
-                ExistingPeriodicWorkPolicy.KEEP,
-                repeatingRequest)
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest)
     }
 }
