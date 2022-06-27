7da38e2e2d7759eb67b02682d008aa9ff671222b
markgray <mark.t.gray@gmail.com>
2022-06-27 07:28:59 -0400

    Made changes to the UI in AboutMeCompose

--- AboutMeCompose/app/src/main/java/com/example/aboutmecompose/MainActivity.kt
+++ AboutMeCompose/app/src/main/java/com/example/aboutmecompose/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
@@ -20,6 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -63,7 +66,7 @@ fun NameNicknameButtonAndFishtail(modifier: Modifier = Modifier) {
         mutableStateOf(true)
     }
     Column(
-        modifier = modifier.verticalScroll(rememberScrollState()),
+        modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
@@ -92,25 +95,33 @@ fun NameNicknameButtonAndFishtail(modifier: Modifier = Modifier) {
             text = nickNameSaved,
             fontSize = 20.sp
         )
+        Spacer(modifier = Modifier.height(20.dp))
         Image(
+            modifier =Modifier.scale(3.0f),
             painter = painterResource(id = android.R.drawable.btn_star_big_on),
             contentDescription = stringResource(id = R.string.yellow_star)
         )
-        Text(
-            text = stringResource(id = R.string.bio),
-            fontSize = 20.sp,
-            modifier = modifier.padding(start = 8.dp, end = 8.dp)
-        )
-        Text(
-            text = stringResource(id = R.string.more_bio1),
-            fontSize = 20.sp,
-            modifier = modifier.padding(start = 8.dp, end = 8.dp)
-        )
-        Text(
-            text = stringResource(id = R.string.more_bio2),
-            fontSize = 20.sp,
-            modifier = modifier.padding(start = 8.dp, end = 8.dp)
-        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Column(
+            modifier = modifier.verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(id = R.string.bio),
+                fontSize = 20.sp,
+                modifier = modifier.padding(start = 8.dp, end = 8.dp)
+            )
+            Text(
+                text = stringResource(id = R.string.more_bio1),
+                fontSize = 20.sp,
+                modifier = modifier.padding(start = 8.dp, end = 8.dp)
+            )
+            Text(
+                text = stringResource(id = R.string.more_bio2),
+                fontSize = 20.sp,
+                modifier = modifier.padding(start = 8.dp, end = 8.dp)
+            )
+        }
     }
 }
 
