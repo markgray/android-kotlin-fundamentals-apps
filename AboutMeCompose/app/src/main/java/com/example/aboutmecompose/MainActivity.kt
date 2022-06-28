2b0e1972e06877c9c76ac77d9dd2899a180c5d5b
markgray <mark.t.gray@gmail.com>
2022-06-28 07:47:38 -0400

    Made clicking on the nickname Text allow you to change nickname in AboutMeCompose

--- AboutMeCompose/app/src/main/java/com/example/aboutmecompose/MainActivity.kt
+++ AboutMeCompose/app/src/main/java/com/example/aboutmecompose/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -24,7 +24,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -95,7 +94,12 @@ fun NameNicknameButtonAndFishtail(modifier: Modifier = Modifier) {
         }
         Text(
             text = nickNameSaved,
-            fontSize = 20.sp
+            fontSize = 20.sp,
+            modifier = Modifier.clickable {
+                showDoneButton = true
+                showEnterNickNameTextField = true
+                nickNameSaved = ""
+            }
         )
         Box(modifier = Modifier
             .height(40.dp)
