9340d061ebd81a3c6888eb722c886ff422e268a0
markgray <mark.t.gray@gmail.com>
2022-06-28 09:12:03 -0400

    Modified skeleton UI to be more usable in ColorMyViewsCompose

--- ColorMyViewsCompose/app/src/main/java/com/example/colormyviewscompose/MainActivity.kt
+++ ColorMyViewsCompose/app/src/main/java/com/example/colormyviewscompose/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.colormyviewscompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.colormyviewscompose.ui.theme.ColorMyViewsComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +20,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ColorMyViewsComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                ColorMyViewApp()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ColorMyViewsComposeTheme {
-        Greeting("Android")
+fun ColorMyViewApp() {
+    ColumnContent(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    )
+}
+
+@Composable
+fun ColumnContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.hello_world),
+            fontSize = 40.sp
+        )
     }
 }
