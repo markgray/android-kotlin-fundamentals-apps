ccd4dafa09c7d7b82c4bea03f338efb4fd1d08aa
markgray <mark.t.gray@gmail.com>
2022-06-28 07:41:18 -0400

    Added a Box around the Image to allow me to set the size of the image in AboutMeCompose

--- AboutMeCompose/app/src/main/java/com/example/aboutmecompose/MainActivity.kt
+++ AboutMeCompose/app/src/main/java/com/example/aboutmecompose/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -95,13 +97,15 @@ fun NameNicknameButtonAndFishtail(modifier: Modifier = Modifier) {
             text = nickNameSaved,
             fontSize = 20.sp
         )
-        Spacer(modifier = Modifier.height(20.dp))
-        Image(
-            modifier =Modifier.scale(3.0f),
-            painter = painterResource(id = android.R.drawable.btn_star_big_on),
-            contentDescription = stringResource(id = R.string.yellow_star)
-        )
-        Spacer(modifier = Modifier.height(20.dp))
+        Box(modifier = Modifier
+            .height(40.dp)
+            .width(40.dp)) {
+            Image(
+                modifier =Modifier.fillMaxSize(1f),
+                painter = painterResource(id = android.R.drawable.btn_star_big_on),
+                contentDescription = stringResource(id = R.string.yellow_star)
+            )
+        }
         Column(
             modifier = modifier.verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
