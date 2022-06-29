e18bcf5f4c4042d2eaa741155908fb7ce9c03ce5
markgray <mark.t.gray@gmail.com>
2022-06-29 08:20:37 -0400

    Fixed size (now need to center it!) in ColorMyViewsCompose

--- ColorMyViewsCompose/app/src/main/java/com/example/colormyviewscompose/MainActivity.kt
+++ ColorMyViewsCompose/app/src/main/java/com/example/colormyviewscompose/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.colormyviewscompose.ui.theme.ColorMyViewsComposeTheme
 
@@ -55,10 +58,10 @@ fun ColumnContent(modifier: Modifier = Modifier) {
         Text(
             text = stringResource(id = R.string.box_one),
             fontSize = 24.sp,
-            modifier = Modifier
-                .fillMaxSize()
-                .wrapContentSize(Alignment.TopCenter)
+            modifier = Modifier.fillMaxWidth()
+                .padding(16.dp)
                 .background(boxOneColor)
+                .padding(16.dp)
                 .clickable {
                     boxOneColor = Color.DarkGray
                 }
