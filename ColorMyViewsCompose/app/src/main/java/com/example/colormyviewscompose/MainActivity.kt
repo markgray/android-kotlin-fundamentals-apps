df51fa75c8003061a00a2472da4741111e23ab59
markgray <mark.t.gray@gmail.com>
2022-06-29 08:10:55 -0400

    Added clickable color change for a Text (size is wrong) in ColorMyViewsCompose

--- ColorMyViewsCompose/app/src/main/java/com/example/colormyviewscompose/MainActivity.kt
+++ ColorMyViewsCompose/app/src/main/java/com/example/colormyviewscompose/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.colormyviewscompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -32,19 +39,29 @@ fun ColorMyViewApp() {
     ColumnContent(
         modifier = Modifier
             .fillMaxSize()
-            .wrapContentSize(Alignment.Center)
+            .wrapContentSize(Alignment.TopCenter)
     )
 }
 
 @Composable
 fun ColumnContent(modifier: Modifier = Modifier) {
+    var boxOneColor by remember {
+        mutableStateOf(Color.White)
+    }
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = stringResource(id = R.string.hello_world),
-            fontSize = 40.sp
+            text = stringResource(id = R.string.box_one),
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.TopCenter)
+                .background(boxOneColor)
+                .clickable {
+                    boxOneColor = Color.DarkGray
+                }
         )
     }
 }
