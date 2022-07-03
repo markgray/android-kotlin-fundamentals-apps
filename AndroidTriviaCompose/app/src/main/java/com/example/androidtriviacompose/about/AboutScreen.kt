52c6ee34d61cfb85dc9f56e0dbb1dac540d8c3a2
markgray <mark.t.gray@gmail.com>
2022-07-03 07:57:04 -0400

    Implemented AboutScreen.kt and RulesScreen.kt in AndroidTriviaCompose

--- AndroidTriviaCompose/app/src/main/java/com/example/androidtriviacompose/about/AboutScreen.kt
+++ AndroidTriviaCompose/app/src/main/java/com/example/androidtriviacompose/about/AboutScreen.kt
@@ -2,5 +2,44 @@
 
 package com.example.androidtriviacompose.about
 
-class AboutScreen {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.androidtriviacompose.R
+
+@Preview(showBackground = true)
+@Composable
+fun AboutScreen(
+    modifier: Modifier = Modifier
+) {
+    AboutScreenContent(modifier)
+}
+
+@Composable
+fun AboutScreenContent(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.about_android_trivia),
+            contentDescription = null)
+        Spacer(modifier = modifier.height(20.dp))
+        Text(
+            text = stringResource(id = R.string.about_text),
+            fontSize = 20.sp
+        )
+    }
 }
--- AndroidTriviaCompose/app/src/main/java/com/example/androidtriviacompose/rules/RulesScreen.kt
+++ AndroidTriviaCompose/app/src/main/java/com/example/androidtriviacompose/rules/RulesScreen.kt
@@ -2,5 +2,44 @@
 
 package com.example.androidtriviacompose.rules
 
-class RulesScreen {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.androidtriviacompose.R
+
+@Preview(showBackground = true)
+@Composable
+fun RulesScreen(
+    modifier: Modifier = Modifier
+) {
+    RulesScreenContent(modifier)
+}
+
+@Composable
+fun RulesScreenContent(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.trivia_rules),
+            contentDescription = null)
+        Spacer(modifier = modifier.height(20.dp))
+        Text(
+            text = stringResource(id = R.string.rules_text),
+            fontSize = 20.sp
+        )
+    }
 }
