fa584f1389155405d95e7f8dd4edcdf2a8e9daed
markgray <mark.t.gray@gmail.com>
2022-07-05 07:51:31 -0400

    Implemented GameWonScreen in AndroidTriviaCompose

--- AndroidTriviaCompose/app/src/main/java/com/example/androidtriviacompose/gamewon/GameWonScreen.kt
+++ AndroidTriviaCompose/app/src/main/java/com/example/androidtriviacompose/gamewon/GameWonScreen.kt
@@ -1,4 +1,49 @@
 package com.example.androidtriviacompose.gamewon
 
-class GameWonScreen {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.androidtriviacompose.R
+
+@Preview
+@Composable
+fun GameWonScreen(
+    modifier: Modifier = Modifier
+) {
+    GameWonScreenContent(modifier = modifier)
+}
+
+@Composable
+fun GameWonScreenContent(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = modifier.height(100.dp))
+        Image(
+            painter = painterResource(id = R.drawable.you_win),
+            contentDescription = null
+        )
+        Spacer(modifier = modifier.height(100.dp))
+        Button(onClick = { /*TODO*/ }) {
+            Text(
+                text = stringResource(id = R.string.next_match),
+                fontSize = 18.sp
+            )
+        }
+    }
 }
--- AndroidTriviaCompose/app/src/main/java/com/example/androidtriviacompose/title/TitleScreen.kt
+++ AndroidTriviaCompose/app/src/main/java/com/example/androidtriviacompose/title/TitleScreen.kt
@@ -19,7 +19,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.androidtriviacompose.R
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun TitleScreen(
     modifier: Modifier = Modifier
