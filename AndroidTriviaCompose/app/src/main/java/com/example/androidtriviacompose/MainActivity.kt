d2551c1003220a803b776a128458e4884b416caa
markgray <mark.t.gray@gmail.com>
2022-07-11 07:08:32 -0400

    Added image to Scaffold drawer buttons in AndroidTriviaCompose

--- AndroidTriviaCompose/app/src/main/java/com/example/androidtriviacompose/MainActivity.kt
+++ AndroidTriviaCompose/app/src/main/java/com/example/androidtriviacompose/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.androidtriviacompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
@@ -28,6 +31,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -116,11 +120,21 @@ fun DrawerContent(navController: NavHostController) {
     Button(onClick = {
         navController.navigate(Routes.Rules.route)
     }) {
+        Image(
+            painter = painterResource(id = R.drawable.rules),
+            contentDescription = stringResource(id = R.string.rules)
+        )
+        Spacer(modifier = Modifier.width(4.dp))
         Text(text = stringResource(id = R.string.rules))
     }
     Button(onClick = {
         navController.navigate(Routes.About.route)
     }) {
+        Image(
+            painter = painterResource(id = R.drawable.about_button),
+            contentDescription = stringResource(id = R.string.about)
+        )
+        Spacer(modifier = Modifier.width(4.dp))
         Text(text = stringResource(id = R.string.about))
     }
 }
