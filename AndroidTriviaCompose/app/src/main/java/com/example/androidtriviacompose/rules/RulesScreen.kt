d162b7f3786e5eb1be61fc6241fd71b59d985a4a
markgray <mark.t.gray@gmail.com>
2022-07-13 08:09:04 -0400

    Added verticalScroll modifier to more column (landscape orientation!) in AndroidTriviaCompose

--- AndroidTriviaCompose/app/src/main/java/com/example/androidtriviacompose/rules/RulesScreen.kt
+++ AndroidTriviaCompose/app/src/main/java/com/example/androidtriviacompose/rules/RulesScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -32,7 +34,7 @@ fun RulesScreenContent(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier.padding(8.dp),
+        modifier = modifier.padding(8.dp).verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
