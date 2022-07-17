f20c21724d1e345e510c0ef78e141e2008a92f4a
markgray <mark.t.gray@gmail.com>
2022-07-17 06:26:00 -0400

    Updated gradle dependencies and kotlin to 1.7.10 in RecyclerViewGridLayout

--- RecyclerViewGridLayout/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ RecyclerViewGridLayout/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -124,7 +124,7 @@ class SleepTrackerFragment : Fragment() {
         // Get a reference to the ViewModel associated with this fragment.
         val sleepTrackerViewModel =
             ViewModelProvider(
-                this, viewModelFactory).get(SleepTrackerViewModel::class.java)
+                this, viewModelFactory)[SleepTrackerViewModel::class.java]
 
         // To use the View Model with data binding, you have to explicitly
         // give the binding object a reference to it.
