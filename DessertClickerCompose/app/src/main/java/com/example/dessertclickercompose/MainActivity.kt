91b524c026587ecdf8e84f614ec1ba00ba945a12
markgray <mark.t.gray@gmail.com>
2022-07-21 08:01:17 -0400

    Added sample ConstraintLayout code in DessertClickerCompose

--- DessertClickerCompose/app/src/main/java/com/example/dessertclickercompose/MainActivity.kt
+++ DessertClickerCompose/app/src/main/java/com/example/dessertclickercompose/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,7 +15,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.dessertclickercompose.ui.theme.DessertClickerComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -54,5 +57,27 @@ fun ColumnContent(modifier: Modifier = Modifier) {
             text = stringResource(id = R.string.hello_world),
             fontSize = 40.sp
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ConstraintLayoutContent() {
+    ConstraintLayout {
+        val (button, text) = createRefs()
+
+        Button(
+            onClick = { /* Do something */ },
+            modifier = Modifier.constrainAs(button) {
+                top.linkTo(parent.top, margin = 16.dp)
+            }
+        ) {
+            Text("Button")
+        }
+
+        Text("Text", Modifier.constrainAs(text) {
+            top.linkTo(button.bottom, margin = 16.dp)
+        })
+
     }
 }
