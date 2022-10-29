f63c4044d7c28bdf28bea305737ceb68c3d9d984
Mark Gray <mark.t.gray@gmail.com>
2022-10-29 07:10:08 -0400

    Added explanations for @Suppress usage and TODOs for deprecations in RecycleViewGridLayout

--- RecyclerViewGridLayout/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ RecyclerViewGridLayout/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -100,7 +100,7 @@ class SleepTrackerFragment : Fragment() {
      * from a previous saved state as given here.
      * @return Return the View for the fragment's UI, or null.
      */
-    @Suppress("RedundantNullableReturnType")
+    @Suppress("RedundantNullableReturnType") // The method we are overriding returns nullable
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -133,12 +133,12 @@ class SleepTrackerFragment : Fragment() {
         val adapter = SleepNightAdapter()
         binding.sleepList.adapter = adapter
 
-        @Suppress("RedundantSamConstructor")
-        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
-            it?.let {
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner) { sleepNightList: List<SleepNight> ->
+            @Suppress("UNNECESSARY_SAFE_CALL") // It starts out empty, so should be nullable?
+            sleepNightList?.let {
                 adapter.submitList(it)
             }
-        })
+        }
 
         // Specify the current activity as the lifecycle owner of the binding.
         // This is necessary so that the binding can observe LiveData updates.
@@ -146,9 +146,8 @@ class SleepTrackerFragment : Fragment() {
 
         // Add an Observer on the state variable for showing a Snackbar message
         // when the CLEAR button is pressed.
-        @Suppress("RedundantSamConstructor")
-        sleepTrackerViewModel.showSnackBarEvent.observe(viewLifecycleOwner, Observer {
-            if (it == true) { // Observed state is true.
+        sleepTrackerViewModel.showSnackBarEvent.observe(viewLifecycleOwner) { showSnackBar: Boolean? ->
+            if (showSnackBar == true) { // Observed state is true.
                 Snackbar.make(
                     requireActivity().findViewById(android.R.id.content),
                     getString(R.string.cleared_message),
@@ -158,11 +157,10 @@ class SleepTrackerFragment : Fragment() {
                 // has a configuration change.
                 sleepTrackerViewModel.doneShowingSnackbar()
             }
-        })
+        }
 
         // Add an Observer on the state variable for Navigating when STOP button is pressed.
-        @Suppress("RedundantSamConstructor")
-        sleepTrackerViewModel.navigateToSleepQuality.observe(viewLifecycleOwner, Observer { night ->
+        sleepTrackerViewModel.navigateToSleepQuality.observe(viewLifecycleOwner) { night: SleepNight? ->
             night?.let {
                 // We need to get the navController from this, because button is not ready, and it
                 // just has to be a view. For some reason, this only matters if we hit stop again
@@ -178,7 +176,7 @@ class SleepTrackerFragment : Fragment() {
                 // has a configuration change.
                 sleepTrackerViewModel.doneNavigating()
             }
-        })
+        }
 
         val manager = GridLayoutManager(activity, 3)
         binding.sleepList.layoutManager = manager
