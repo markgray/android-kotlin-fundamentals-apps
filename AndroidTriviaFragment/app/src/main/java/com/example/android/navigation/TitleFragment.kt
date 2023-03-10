8a8ed4b9303a3a63ddf2c6928214cbb235d0d760
markgray <mark.t.gray@gmail.com>
2023-03-10 05:03:24 -0500

    Removed unused const symbols in AndroidTriviaFragment

--- AndroidTriviaFragment/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTriviaFragment/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -24,13 +24,6 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-@Suppress("unused") // TODO: Rename parameter arguments, choose names that match
-private const val ARG_PARAM1 = "param1"
-
-@Suppress("unused") // TODO: Rename parameter arguments, choose names that match
-private const val ARG_PARAM2 = "param2"
-
 /**
  * The `startDestination` [Fragment] of our app, it just displays our Title "Android Trivia", and
  * a "Play" button which will navigate to the `GameFragment` in a later codelab when clicked.
