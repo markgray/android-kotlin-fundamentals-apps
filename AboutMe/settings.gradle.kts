9dd86618b6e03ae6a563cde32568e2ce10c39f13
markgray <mark.t.gray@gmail.com>
2024-06-04 06:12:56 -0400

    Converted settings.gradle to kts, and updated gradle dependency in AboutMe

--- AboutMe/settings.gradle.kts
+++ AboutMe/settings.gradle.kts
@@ -13,5 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 
-include ':app'
+include (":app")
