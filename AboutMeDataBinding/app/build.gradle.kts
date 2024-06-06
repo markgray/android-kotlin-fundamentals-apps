e9c25ffd582bf3b2fe69da79ee0bbe5d719a927b
markgray <mark.t.gray@gmail.com>
2024-06-06 04:27:08 -0400

    Tidied up build files in AboutMeDataBinding

--- AboutMeDataBinding/app/build.gradle.kts
+++ AboutMeDataBinding/app/build.gradle.kts
@@ -17,7 +17,10 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
-            proguardFiles (getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
