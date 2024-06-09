c02797bce3be933f146f02a0234c92f6917087e7
markgray <mark.t.gray@gmail.com>
2024-06-09 06:10:27 -0400

    Converted build files to kts in GuessTheWordLiveData

--- GuessTheWordLiveData/settings.gradle.kts
+++ GuessTheWordLiveData/settings.gradle.kts
@@ -14,4 +14,18 @@
  * limitations under the License.
  */
 
-include ':app'
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage") // TODO: Keep an eye on this
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+include(":app")
