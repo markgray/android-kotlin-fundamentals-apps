9d28db9f6de46754f7b9d5be6c6c3d6ec03508a7
markgray <mark.t.gray@gmail.com>
2024-09-11 05:21:52 -0400

    Updated gradle dependencies in MarsRealEstateNetwork

--- MarsRealEstateNetwork/app/build.gradle.kts
+++ MarsRealEstateNetwork/app/build.gradle.kts
@@ -64,8 +64,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.0")
 
     // Core with Ktx
     implementation("androidx.core:core-ktx:1.13.1")
