d280872ab7d413db617aa4c1ea5c2fa0e3c12e7f
Mark Gray <mark.t.gray@gmail.com>
2024-09-17 04:37:08 -0400

    Corrected margins in AndroidTriviaNavigation

--- AndroidTriviaNavigation/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ AndroidTriviaNavigation/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -17,8 +17,12 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
@@ -66,6 +70,23 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
 
         drawerLayout = binding.drawerLayout
+        ViewCompat.setOnApplyWindowInsetsListener(drawerLayout) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // Apply the insets as a margin to the view. This solution sets
+            // only the bottom, left, and right dimensions, but you can apply whichever
+            // insets are appropriate to your layout. You can also update the view padding
+            // if that's more appropriate.
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = insets.left
+                bottomMargin = insets.bottom
+                rightMargin = insets.right
+                topMargin = insets.top
+            }
+
+            // Return CONSUMED if you don't want want the window insets to keep passing
+            // down to descendant views.
+            WindowInsetsCompat.CONSUMED
+        }
 
         val navController = this.findNavController(R.id.myNavHostFragment)
 
