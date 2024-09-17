d14ff00e47aad3a0e3b30805029c3d052c29b874
Mark Gray <mark.t.gray@gmail.com>
2024-09-17 03:51:15 -0400

    Corrected margins in AboutMe

--- AboutMe/app/src/main/java/com/android/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/android/example/aboutme/MainActivity.kt
@@ -16,9 +16,14 @@
 
 package com.android.example.aboutme
 
-import androidx.activity.ComponentActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 
 /**
  * Main Activity of the AboutMe app. This app demonstrates:
@@ -37,5 +42,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rootView: LinearLayout = findViewById(R.id.root_view)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // Apply the insets as a margin to the view. This solution sets
+            // only the bottom, left, and right dimensions, but you can apply whichever
+            // insets are appropriate to your layout. You can also update the view padding
+            // if that's more appropriate.
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = insets.left
+                bottomMargin = insets.bottom
+                rightMargin = insets.right
+                topMargin = insets.top
+            }
+
+            // Return CONSUMED if you don't want want the window insets to keep passing
+            // down to descendant views.
+            WindowInsetsCompat.CONSUMED
+        }
     }
 }
