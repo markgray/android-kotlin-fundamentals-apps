a0409c9c5fca09aabe59fa7be77fd6c996cc19db
markgray <mark.t.gray@gmail.com>
2024-09-17 05:49:26 -0400

    Added enableEdgeToEdge in DevBytesWorkManager

--- DevBytesWorkManager/app/src/main/java/com/example/android/devbyteviewer/ui/DevByteActivity.kt
+++ DevBytesWorkManager/app/src/main/java/com/example/android/devbyteviewer/ui/DevByteActivity.kt
@@ -17,7 +17,13 @@
 package com.example.android.devbyteviewer.ui
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import com.example.android.devbyteviewer.R
 
 /**
@@ -35,6 +41,25 @@ class DevByteActivity : AppCompatActivity() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_dev_byte_viewer)
+        val rootView = findViewById<FrameLayout>(R.id.root_view)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // Apply the insets as a margin to the view. This solution sets
+            // only the bottom, left, and right dimensions, but you can apply whichever
+            // insets are appropriate to your layout. You can also update the view padding
+            // if that's more appropriate.
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = insets.left
+                bottomMargin = insets.bottom
+                rightMargin = insets.right
+                topMargin = insets.top
+            }
+
+            // Return CONSUMED if you don't want want the window insets to keep passing
+            // down to descendant views.
+            WindowInsetsCompat.CONSUMED
+        }
     }
 }
