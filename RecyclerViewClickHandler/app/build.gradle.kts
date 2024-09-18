dd91bd9250daa6d6a71b1afface12fa4bfbcd5a4
markgray <mark.t.gray@gmail.com>
2024-09-18 06:39:04 -0400

    Updated AGP to 8.6.1, and added enableEdgeToEdge in RecyclerViewClickHandler

--- RecyclerViewClickHandler/app/build.gradle.kts
+++ RecyclerViewClickHandler/app/build.gradle.kts
@@ -60,6 +60,7 @@ android {
 dependencies {
     // Support libraries
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.activity:activity-ktx:1.9.2")
     implementation("androidx.fragment:fragment-ktx:1.8.3")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
