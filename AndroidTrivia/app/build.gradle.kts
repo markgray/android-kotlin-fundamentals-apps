9af73b7204f1903936719c55484d849dbea6143c
Mark Gray <mark.t.gray@gmail.com>
2024-10-05 04:16:59 -0400

    Updated AGP to 8.7.0, and updated gradle dependencies in AndroidTrivia

--- AndroidTrivia/app/build.gradle.kts
+++ AndroidTrivia/app/build.gradle.kts
@@ -66,7 +66,7 @@ dependencies {
     androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.navigation:navigation-runtime-ktx:2.8.1")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.1")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.1")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.8.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.2")
 }
