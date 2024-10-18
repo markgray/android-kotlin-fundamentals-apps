fe2fe10e3b5928693eb28692a46558ddfaf5b7a2
markgray <mark.t.gray@gmail.com>
2024-10-18 05:14:22 -0400

    Updated AGP to 8.7.1, and updated gradle dependencies in GDGFinderMaterial

--- GDGFinderMaterial/app/build.gradle.kts
+++ GDGFinderMaterial/app/build.gradle.kts
@@ -64,8 +64,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.2")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.3")
 
     // Core with Ktx
     implementation("androidx.core:core-ktx:1.13.1")
@@ -84,7 +84,7 @@ dependencies {
 
     // material design components
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.activity:activity-ktx:1.9.3")
 
     // Moshi
     implementation("com.squareup.moshi:moshi:1.15.1")
