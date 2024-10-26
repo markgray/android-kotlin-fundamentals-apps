0229796a4bb935d328a2e7cad3e7d5fc147cbd88
Mark Gray <mark.t.gray@gmail.com>
2024-10-26 02:49:24 -0400

    Updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk t0 35 and updated gradle dependency in DesertClickerFinal

--- DessertClickerFinal/app/build.gradle.kts
+++ DessertClickerFinal/app/build.gradle.kts
@@ -21,11 +21,11 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.example.android.dessertclicker"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -55,7 +55,7 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.activity:activity-ktx:1.9.3")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.jakewharton.timber:timber:5.0.1")
 }
