58928685ad6fe698dc4473181abb68232fd1681e
markgray <mark.t.gray@gmail.com>
2024-12-08 07:07:32 -0500

    Updated gradle dependency in AndroidTrivia

--- AndroidTrivia/app/build.gradle.kts
+++ AndroidTrivia/app/build.gradle.kts
@@ -57,7 +57,7 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.21")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.1.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.activity:activity-ktx:1.9.3")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
