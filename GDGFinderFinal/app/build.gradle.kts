06f6793af3c730c542560ee855cb7d831197b156
Mark Gray <mark.t.gray@gmail.com>
2024-12-20 04:30:19 -0500

    Updated gradle dependencies in GDGFinderFinal

--- GDGFinderFinal/app/build.gradle.kts
+++ GDGFinderFinal/app/build.gradle.kts
@@ -71,8 +71,8 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.9.3")
 
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.5")
 
     // Core with Ktx
     implementation("androidx.core:core-ktx:1.15.0")
