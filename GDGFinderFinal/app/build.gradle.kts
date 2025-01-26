a4e6613ec767f0afc653c749feb33b94f12ca7e5
Mark Gray <mark.t.gray@gmail.com>
2025-01-26 04:50:17 -0500

    Updated gradle dependency and fixed warnings in GDGFinderFinal

--- GDGFinderFinal/app/build.gradle.kts
+++ GDGFinderFinal/app/build.gradle.kts
@@ -64,11 +64,11 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
 
     // RecyclerView
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
 
     // material design components
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("androidx.activity:activity-ktx:1.10.0")
 
     // Navigation
     implementation("androidx.navigation:navigation-fragment-ktx:2.8.5")
