ca5c74e4fba0fbeca2a898c85e54bdf1db968d1a
Mark Gray <mark.t.gray@gmail.com>
2025-02-22 04:00:30 -0500

    Updated AGP to 8.8.1 and updated gradle dependencies in AndroidTriviaNavigation

--- AndroidTriviaNavigation/app/build.gradle.kts
+++ AndroidTriviaNavigation/app/build.gradle.kts
@@ -55,7 +55,7 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect:2.1.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.1.10")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.activity:activity-ktx:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
@@ -63,8 +63,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    implementation("androidx.navigation:navigation-runtime-ktx:2.8.6")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.6")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.6")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.8.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.7")
     implementation("com.google.android.material:material:1.12.0")
 }
