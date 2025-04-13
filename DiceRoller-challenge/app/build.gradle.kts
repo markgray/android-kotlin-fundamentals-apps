bd8e86a615c4fe1cbc24151ea4037910d2a30732
markgray <mark.t.gray@gmail.com>
2025-04-13 05:54:19 -0400

    Updated gradle dependency in DiceRoller-challenge

--- DiceRoller-challenge/app/build.gradle.kts
+++ DiceRoller-challenge/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.activity:activity-ktx:1.10.1")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:runner:1.6.2")
