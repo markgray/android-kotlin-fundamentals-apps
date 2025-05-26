8bf1491830041e9934818e06aa42dc974459482d
markgray <mark.t.gray@gmail.com>
2025-05-26 07:31:26 -0400

    Updated kotlin to 2.1.21 and updated gradle dependency in RecyclerViewClickHandler

--- RecyclerViewClickHandler/app/build.gradle.kts
+++ RecyclerViewClickHandler/app/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     // Support libraries
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.activity:activity-ktx:1.10.1")
-    implementation("androidx.fragment:fragment-ktx:1.8.6")
+    implementation("androidx.fragment:fragment-ktx:1.8.7")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 
     // Android KTX
