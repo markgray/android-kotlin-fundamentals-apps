e0cd515eac56cd516baf9d826654a55255e4ce15
Mark Gray <mark.t.gray@gmail.com>
2025-05-26 04:13:06 -0400

    Updated kotlin to 2.1.21 in GDGFinderFinal

--- GDGFinderFinal/build.gradle.kts
+++ GDGFinderFinal/build.gradle.kts
@@ -2,7 +2,7 @@
 
 plugins {
     id("com.android.application") version "8.10.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
-    id("org.jetbrains.kotlin.plugin.parcelize") version "2.1.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
+    id("org.jetbrains.kotlin.plugin.parcelize") version "2.1.21" apply false
     id("androidx.navigation.safeargs") version "2.9.0" apply false
 }
