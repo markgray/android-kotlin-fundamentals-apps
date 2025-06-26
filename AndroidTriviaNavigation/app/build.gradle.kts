14c41f05e530115a9e7d16d4c960b2d907274b19
markgray <mark.t.gray@gmail.com>
2025-06-26 06:56:41 -0400

    Updated targetSdk to 36, updated AGP to 8.11.0, updated gradle to 8.14.2, updated kotlin to 2.2.0, switched to "kotlin compilerOptions" and updated gradle dependency in AndroidTriviaNavigation

--- AndroidTriviaNavigation/app/build.gradle.kts
+++ AndroidTriviaNavigation/app/build.gradle.kts
@@ -1,18 +1,4 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("com.android.application")
@@ -21,14 +7,14 @@ plugins {
 }
 
 android {
-    compileSdk = 35
+    compileSdk = 36
     buildFeatures {
         dataBinding = true
     }
     defaultConfig {
         applicationId = "com.example.android.navigation"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
         vectorDrawables.useSupportLibrary = true
         versionCode = 1
         versionName = "1.0"
@@ -47,15 +33,16 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
     }
-
     namespace = "com.example.android.navigation"
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect:2.1.21")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.2.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
