31be6cc5b304dd81ab28b800b0741eeec173b064
Mark Gray <mark.t.gray@gmail.com>
2025-08-07 03:29:27 -0400

    Updated AGP to 8.12.0, updated gradle to 9.0.0, and updated gradle dependencies in MarsRealEstateNetwork

--- MarsRealEstateNetwork/app/build.gradle.kts
+++ MarsRealEstateNetwork/app/build.gradle.kts
@@ -52,8 +52,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.9.2")
-    implementation("androidx.navigation:navigation-ui-ktx:2.9.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.3")
 
     // Core with Ktx
     implementation("androidx.core:core-ktx:1.16.0")
