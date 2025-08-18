929d8de962bd4cf36f1fe57a17cace202e5529ea
markgray <mark.t.gray@gmail.com>
2025-08-18 06:55:25 -0400

    Updated kotlin to 2.2.10, fixed warnings, and updated gradle dependencies in AboutMeInteractive

--- AboutMeInteractive/app/build.gradle.kts
+++ AboutMeInteractive/app/build.gradle.kts
@@ -34,12 +34,10 @@ android {
         }
     }
     namespace = "com.android.example.aboutme"
-
 }
-
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.activity:activity-ktx:1.10.1")
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 }
