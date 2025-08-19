f588e046f5d099cb2f46edd78de11dd9fc933552
Mark Gray <mark.t.gray@gmail.com>
2025-08-19 04:17:25 -0400

    Updated AGP to 8.12.1, updated kotlin to 2.2.10, fixed warnings, and updated gradle dependencies in DiceRoller

--- DiceRoller/app/build.gradle.kts
+++ DiceRoller/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.activity:activity-ktx:1.10.1")
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:runner:1.7.0")
