5d0b2680386b981318c5c064e6a712bce3b84864
Mark Gray <mark.t.gray@gmail.com>
2025-08-19 03:59:41 -0400

    Updated AGP to 8.12.1, updated kotlin to 2.2.10, fixed warnings, and updated gradle dependencies in DevBytesRepository

--- DevBytesRepository/app/build.gradle.kts
+++ DevBytesRepository/app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
 
     // Android KTX
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
 
     // constraint layout
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
