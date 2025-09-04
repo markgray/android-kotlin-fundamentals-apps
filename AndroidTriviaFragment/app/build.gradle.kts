5a352e9ca08b91d4ac4446c78af1353c4384b0e6
markgray <mark.t.gray@gmail.com>
2025-09-04 07:57:01 -0400

    Updated AGP to 8.13.0, fixed warnings, and updated gradle dependencies in AndroidTriviaFragment

--- AndroidTriviaFragment/app/build.gradle.kts
+++ AndroidTriviaFragment/app/build.gradle.kts
@@ -66,5 +66,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:runner:1.7.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.13.0")
 }
