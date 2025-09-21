add04d99b30c335e8c9ae5944ea84f5046e1b834
markgray <mark.t.gray@gmail.com>
2025-09-21 05:25:31 -0400

    Updated kotlin to 2.2.20, fixed warnings, and updated gradle dependencies in AboutMeDataBinding

--- AboutMeDataBinding/app/build.gradle.kts
+++ AboutMeDataBinding/app/build.gradle.kts
@@ -42,6 +42,6 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.activity:activity-ktx:1.11.0")
     testImplementation("junit:junit:4.13.2")
 }
