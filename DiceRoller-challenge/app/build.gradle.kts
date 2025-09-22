b690ae9460d10be1629b5240adea4696c7b9564d
Mark Gray <mark.t.gray@gmail.com>
2025-09-22 03:14:12 -0400

    Updated kotlin to 2.2.20, fixed warnings, and updated gradle dependencies in DiceRoller-challenge

--- DiceRoller-challenge/app/build.gradle.kts
+++ DiceRoller-challenge/app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.activity:activity-ktx:1.11.0")
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     testImplementation("junit:junit:4.13.2")
