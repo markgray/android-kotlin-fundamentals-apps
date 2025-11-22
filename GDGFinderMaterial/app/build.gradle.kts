306e11d2fd911808ac02c91fd4aeede3b1a4ff28
markgray <mark.t.gray@gmail.com>
2025-11-22 06:16:59 -0500

    Updated updated gradle dependencies and increased minSdk to 23 in GDGFinderMaterial

--- GDGFinderMaterial/app/build.gradle.kts
+++ GDGFinderMaterial/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     }
     defaultConfig {
         applicationId = "com.example.android.gdgfinder"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -68,11 +68,11 @@ dependencies {
     // ViewModel and LiveData
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
     // use viewModelScope from lifecycle-viewmodel-ktx
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
 
     // material design components
     implementation("com.google.android.material:material:1.13.0")
-    implementation("androidx.activity:activity-ktx:1.11.0")
+    implementation("androidx.activity:activity-ktx:1.12.0")
 
     // Moshi
     implementation("com.squareup.moshi:moshi:1.15.2")
