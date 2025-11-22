1063f69898afe9ee68b826d73ffe79f15e55aa66
Mark Gray <mark.t.gray@gmail.com>
2025-11-22 01:30:13 -0500

    Updated AGP to 8.13.1, and updated gradle dependencies and increased minSdk to 23 in ColorMyViews

--- ColorMyViews/app/build.gradle.kts
+++ ColorMyViews/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 36
     defaultConfig {
         applicationId = "com.android.example.colormyviews"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -44,6 +44,6 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.activity:activity-ktx:1.11.0")
+    implementation("androidx.activity:activity-ktx:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 }
