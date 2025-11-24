7db90d9bf38712b64c389bac1c9a648bbbb388db
markgray <mark.t.gray@gmail.com>
2025-11-24 04:13:56 -0500

    Added comments in AboutMeInteractive

--- AboutMeInteractive/app/src/main/java/com/android/example/aboutme/MainActivity.kt
+++ AboutMeInteractive/app/src/main/java/com/android/example/aboutme/MainActivity.kt
@@ -27,6 +27,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.graphics.Insets
+import androidx.core.view.OnApplyWindowInsetsListener
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -41,12 +43,25 @@ import androidx.core.view.updateLayoutParams
 class MainActivity : ComponentActivity() {
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of [onCreate],
-     * then we set our content view to our layout file R.layout.activity_main. We find the [Button]
-     * with ID R.id.done_button (labeled "Done") and set its `OnClickListener` to a lambda which calls
-     * our method [addNickname] with the view that was clicked, and we find the [TextView] with ID
-     * R.id.nickname_text and set its `OnClickListener` to a lambda which calls our method
-     * [updateNickname] with the view that was clicked.
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to edge
+     * display, then we call our super's implementation of `onCreate`, then we set our content view
+     * to our layout file `R.layout.activity_main`. We initialize our [LinearLayout] variable
+     * `rootView` by finding the view with ID  `R.id.root_view`. We use the
+     * [ViewCompat.setOnApplyWindowInsetsListener] method to set an [OnApplyWindowInsetsListener] to
+     * take over over the policy for applying window insets to `rootView`, with the`listener` argument
+     * a lambda that accepts the [View] passed the lambda in variable `v` and the [WindowInsetsCompat]
+     * passed the lambda in variable `windowInsets`. It initializes its [Insets] variable `insets`
+     * to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates the layout parameters
+     * of `v` to be a [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`, the
+     * right margin set to `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED] to the caller (so
+     * that the window insets will not keep passing down to descendant views).
+     *
+     * Next we find the [Button] with ID `R.id.done_button` (labeled "Done") and set its
+     * `OnClickListener` to a lambda which calls our method [addNickname] with the view that was
+     * clicked, and we find the [TextView] with ID `R.id.nickname_text` and set its `OnClickListener`
+     * to a lambda which calls our method [updateNickname] with the view that was clicked.
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use
      */
@@ -56,7 +71,7 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
         val rootView: LinearLayout = findViewById(R.id.root_view)
         ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
