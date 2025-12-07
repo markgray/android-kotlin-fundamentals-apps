74147f72006553dde0cee52333faae50efbed4b3
Mark Gray <mark.t.gray@gmail.com>
2025-12-06 22:50:47 -0500

    Updated gradle dependencies in DevBytesRepository

--- DevBytesRepository/app/build.gradle.kts
+++ DevBytesRepository/app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
 
     // support libraries
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.activity:activity-ktx:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.12.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("com.google.android.material:material:1.13.0")
 
