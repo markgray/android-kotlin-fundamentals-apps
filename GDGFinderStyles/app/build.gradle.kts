fca203886d7f9dc0b9e863c6cec39bbd42813d00
Mark Gray <mark.t.gray@gmail.com>
2025-12-07 01:03:58 -0500

    Updated gradle dependencies in GDGFinderStyles

--- GDGFinderStyles/app/build.gradle.kts
+++ GDGFinderStyles/app/build.gradle.kts
@@ -46,7 +46,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.12.1")
     // Constraint Layout
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 
