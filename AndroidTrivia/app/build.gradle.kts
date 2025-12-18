4f81269b6e710403bc95b7ebbef664bc57fc4b12
Mark Gray <mark.t.gray@gmail.com>
2025-12-18 04:30:19 -0500

    Updated kotlin to 2.3.0 and updated gradle dependency in AndroidTrivia

--- AndroidTrivia/app/build.gradle.kts
+++ AndroidTrivia/app/build.gradle.kts
@@ -46,9 +46,9 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect:2.2.21")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.3.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.activity:activity-ktx:1.12.1")
+    implementation("androidx.activity:activity-ktx:1.12.2")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     testImplementation("junit:junit:4.13.2")
